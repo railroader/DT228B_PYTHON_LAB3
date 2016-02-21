87d8a0bfe55b16ee6f8b8cae852342b8ba832d94
railroader <terryslattery@me.com>
2016-02-21 13:12:23 +0000

    added comments

--- DT228B_LIB/BruteForcer.py
+++ DT228B_LIB/BruteForcer.py
@@ -1,6 +1,10 @@
 import win32com.client as win32
 
+
+#class to brute force a password protected wordfile
 class BruteForcer():
+    #public private properties below
+
     _wordList = ""
     _fileToCrack = ""
 
@@ -25,35 +29,55 @@ class BruteForcer():
     def __init__(self):
         pass
 
+
+    #method to crack the pin
     def crackFile(self):
-        word = win32.Dispatch("Word.Application")
-        filename = self._fileToCrack
+        word = win32.Dispatch("Word.Application")#call the win32.Dispatch with looks like a static method returning an object
+        '''
+        open wordlist and read the values into a object of type listOf<string>
+        and the strip out the \n
+        '''
         password_file = open(self._wordList, 'r')
         passwords = password_file.readlines()
         password_file.close()
         passwords = [item.rstrip('\n') for item in passwords]
 
+        '''
+        iterate through the list and pass the current value in the password list to the open method of the word doc
+        if the document opens then return the password
+        '''
+
         for password in passwords:
             try:
-                doc = word.Documents.Open(filename,False,True,None,password)
+                doc = word.Documents.Open(self._fileToCrack,False,True,None,password)
                 doc.Close()
                 return str(password)
             except Exception as error:
                 pass
         return ""
 
+
+    #Decided to make the method static as i felt the object does not need to be instantiated
     @staticmethod
     def crackFile(wordlist,WordDocToCrack):
-        word = win32.Dispatch("Word.Application")
-        filename = WordDocToCrack
+        word = win32.Dispatch("Word.Application") #call the win32.Dispatch with looks like a static method returning an object
+        '''
+        open wordlist and read the values into a object of type listOf<string>
+        and the strip out the \n
+        '''
         password_file = open(wordlist, 'r')
         passwords = password_file.readlines()
         password_file.close()
         passwords = [item.rstrip('\n') for item in passwords]
 
+        '''
+        iterate through the list and pass the current value in the password list to the open method of the word doc
+        if the document opens then return the password
+        '''
+
         for password in passwords:
             try:
-                doc = word.Documents.Open(filename,False,True,None,password)
+                doc = word.Documents.Open(WordDocToCrack,False,True,None,password)
                 doc.Close()
                 return str(password)
             except Exception as error:
--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
-import win32com.client as win32
+import win32com.client as win32 #download at https://sourceforge.net/projects/pywin32/files/
 import datetime
 import sys
 import os
-from DT228B_LIB.MyEncryptor import MyEncryptor
-from DT228B_LIB.BruteForcer import BruteForcer
+from DT228B_LIB.MyEncryptor import MyEncryptor #import the encryptor so we can use it here
+from DT228B_LIB.BruteForcer import BruteForcer #import the bruteforcer so we can use it here
 
 
 def conclusion(val1,val2):
@@ -15,29 +15,29 @@ def conclusion(val1,val2):
         print "Conclusion :"
         print "Both before and after hash values are not the same "
 
-
+#Function to kick it all off
 def start():
-    fileToEncode = "C:\Users\Terry\PycharmProjects\lab3\DT228B_LAB3\MDF-SF1.docx"
-    wordlist = r"C:\Users\Terry\PycharmProjects\lab3\DT228B_LAB3\4digits.txt"
+    fileToEncode = "C:\Users\Terry\PycharmProjects\lab3\DT228B_LAB3\MDF-SF1.docx" #File the we want to crack
+    wordlist = r"C:\Users\Terry\PycharmProjects\lab3\DT228B_LAB3\4digits.txt" #wordlist with all the passwords
     print "Getting hash of " + fileToEncode
-    originalHash = MyEncryptor.sha1Encode(fileToEncode)
+    originalHash = MyEncryptor.sha1Encode(fileToEncode) #get the sha1 hash of the file
     print "The Sha1Encode value of the file is  : " ,originalHash
 
     print "Attempting to crack file " + fileToEncode
-    print "Starting Brute force : ", datetime.datetime.now().time()
-    result = BruteForcer.crackFile(wordlist,fileToEncode)
+    print "Starting Brute force : ", datetime.datetime.now().time() #print current time
+    result = BruteForcer.crackFile(wordlist,fileToEncode) #get the pin
 
-    print "Password is " ,result
-    print "Finished Brute force : ", datetime.datetime.now().time()
+    print "Password is " ,result #print the password
+    print "Finished Brute force : ", datetime.datetime.now().time() #print the current time
     print "---------------------------"
     print "Hashing file again"
     print "Starting hash again : ", datetime.datetime.now().time()
     print "Getting hash of " + fileToEncode
-    secondHashValue = MyEncryptor.sha1Encode(fileToEncode)
+    secondHashValue = MyEncryptor.sha1Encode(fileToEncode) #get the sha1 hash of the file
     print "The Sha1Encode value of the file is  : " ,secondHashValue
     print "Finished Hash : ", datetime.datetime.now().time()
 
     print "Generating results"
-    conclusion(originalHash,secondHashValue)
+    conclusion(originalHash,secondHashValue) #call conclusion function to compair the two hashes
 
-start()
+start() #call the start function
